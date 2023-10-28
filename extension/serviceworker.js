39fba7a70c36c0e182ce464777c8da913ff83e62
Vedanshu Joshi <joshvr01@pfw.edu>
2023-10-27 23:56:48 -0400

    Added Comments

--- extension/serviceworker.js
+++ extension/serviceworker.js
@@ -1,13 +1,19 @@
-// Add a listener to trigger the opening of a new tab
-console.log(chrome)
+// Add a listener to respond to messages received by the extension
+console.log(chrome);
+
+// Listen for messages sent from the content script or other parts of the extension
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
     // 2. A page requested user data, respond with a copy of `user`
+
+    // Check if the received message is 'onclicked'
     if (message === 'onclicked') {
-        var tab = await chrome.tabs.query({"active":true});
+        // Query the active tab to get information about it
+        var tab = await chrome.tabs.query({ "active": true });
+
+        // Log the pending URL of the active tab
         console.log(tab[0].pendingUrl);
+
+        // Send a response back to the sender (the content script or extension component that requested data)
         sendResponse("done");
     }
 });
-
-
-
