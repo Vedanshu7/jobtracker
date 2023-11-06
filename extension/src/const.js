0c453be6f5c3bb125265ea97b5fbb528b72a9444
Surbhi Sharma <surbhisharma9099@gmail.com>
2023-11-05 23:44:25 -0500

    REFINED GOOGLE SSO
    Refined Google SSO WITH MV3

---
+++ extension/src/const.js
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCaYs7w0nPuT8HrKUdBzT9ai6OmZKTSUzM",
+  authDomain: "jobtracker-dda65.firebaseapp.com",
+  projectId: "jobtracker-dda65",
+  storageBucket: "jobtracker-dda65.appspot.com",
+  messagingSenderId: "919116326702",
+  appId: "1:919116326702:web:0ad70f9ed9e55fae7c7cbf",
+  measurementId: "G-G9RWZ5739E",
+};
--- extension/src/index.js
+++ extension/src/index.js
@@ -1,30 +1,67 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithCredential,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { FIREBASE_CONFIG } from "./const";
 import reportWebVitals from "./reportWebVitals";
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-const firebaseConfig = {
-  apiKey: "AIzaSyAI0SsYKCExiEUbSsBzJ95yrxFFtdoGj3M",
-  authDomain: "chromeauthentication-79194.firebaseapp.com",
-  projectId: "chromeauthentication-79194",
-  storageBucket: "chromeauthentication-79194.appspot.com",
-  messagingSenderId: "197432855519",
-  appId: "1:197432855519:web:40934fd86992915c7bd26a",
-  measurementId: "G-LPN5Y0QQEV"
+
+export const firebase = initializeApp(FIREBASE_CONFIG);
+export const auth = getAuth(firebase);
+
+export const App = (props) => {
+  const [user, setUser] = React.useState(undefined);
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    chrome.identity.getAuthToken({ interactive: true }, (token) => {
+      if (chrome.runtime.lastError || !token) {
+        alert(
+          `SSO ended with an error: ${JSON.stringify(chrome.runtime.lastError)}`
+        );
+        return;
+      }
+
+      signInWithCredential(auth, GoogleAuthProvider.credential(null, token))
+        .then((res) => {
+          console.log("signed in!");
+        })
+        .catch((err) => {
+          alert(`SSO ended with an error: ${err}`);
+        });
+    });
+  };
+
+  React.useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user && user.uid ? user : null);
+    });
+  }, []);
+
+  if (undefined === user) return <h1>Loading...</h1>;
+
+  if (user != null) {
+    alert(JSON.stringify(user));
+    return (
+      <div>
+        <h1>Signed in as {user.email}.</h1>
+        <button onClick={auth.signOut.bind(auth)}>Sign Out?</button>
+      </div>
+    );
+  } else {
+    return <button onClick={signIn}>Sign In with Google</button>;
+  }
 };
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
