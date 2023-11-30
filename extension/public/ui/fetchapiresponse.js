31b4f4c1f95e65d9dd82f9da9090b910240b14fa
Surbhi Sharma <surbhisharma9099@gmail.com>
2023-11-29 21:42:47 -0500

    Applied filtered functionality
    Have Applied filtered functionality on basis of job keyword and on basis of location filtered jobs are displayed on job tiles.
    I am updating the job on job tiles dynamically from api response

--- extension/public/ui/fetchapiresponse.js
+++ extension/public/ui/fetchapiresponse.js
@@ -1,58 +1,81 @@
 // script.js
 
-function fetchAndDisplayData() {
-    // Fetch the API data
-    fetch("https://localhost:7100/User?userId=76848a5d-1b02-4fbd-954a-0b505bb46f85")
-      .then(response => response.json())
-      .then(data => {
-        // Process the API response and update the job tiles
-        updateJobTiles(data);
-      })
-      .catch(error => console.error("Error fetching API:", error));
+export function fetchAndDisplayData() {
+  // Fetch the API data
+  fetch(
+    "https://localhost:7100/User?userId=76848a5d-1b02-4fbd-954a-0b505bb46f85"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Process the API response and update the job tiles
+      updateJobTiles(data);
+    })
+    .catch((error) => console.error("Error fetching API:", error));
+}
+export function getStatusLabel(status) {
+  switch (status) {
+    case 1:
+      return "Applied";
+    case 2:
+      return "In Review";
+    case 3:
+      return "Accepted";
+    default:
+      return "Apply Now";
   }
-  
-  function updateJobTiles(apiResponse) {
-    // Clear existing job tiles
-    document.getElementById("tab-1").innerHTML = "";
-  
-    // Iterate over the API response and fill job tiles
-    apiResponse.forEach(job => {
-      // Create a new job item
-      var jobItem = document.createElement("div");
-      jobItem.className = "job-item p-4 mb-4";
-  
-      // Create the job item content
-      var content = `
+}
+
+export function updateJobTiles(apiResponse) {
+  // Clear existing job tiles
+  document.getElementById("tab-1").innerHTML = "";
+
+  // Iterate over the API response and fill job tiles
+  apiResponse.forEach((job) => {
+    // Create a new job item
+    var jobItem = document.createElement("div");
+    jobItem.className = "job-item p-4 mb-4";
+
+    // Create the job item content
+    var content = `
         <div class="row g-4">
           <div class="col-sm-12 col-md-8 d-flex align-items-center">
-            <img class="flex-shrink-0 img-fluid border rounded" src="${job.imageUrl}" alt="" style="width: 80px; height: 80px;">
+            <img class="flex-shrink-0 img-fluid border rounded" src="${
+              job.imageUrl
+            }" alt="" style="width: 80px; height: 80px;">
             <div class="text-start ps-4">
               <h5 class="mb-3">${job.title}</h5>
-              <span class="text-truncate me-3"><i class="fa fa-map-marker-alt text-primary me-2"></i>${job.location}</span>
+              <span class="text-truncate me-3"><i class="fa fa-map-marker-alt text-primary me-2"></i>${
+                job.location
+              }</span>
               
-              <span class="text-truncate me-0"><i class="far fa-money-bill-alt text-primary me-2"></i>${job.salary}</span>
+              <span class="text-truncate me-0"><i class="far fa-money-bill-alt text-primary me-2"></i>${
+                job.salary
+              }</span>
             </div>
           </div>
           <div class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center">
             <div class="d-flex mb-3">
               <a class="btn btn-light btn-square me-3" href=""><i class="far fa-heart text-primary"></i></a>
-              <a class="btn btn-primary" href="">Apply Now</a>
+              <a class="btn btn-primary" href="">${getStatusLabel(
+                job.status
+              )}</a>
+              
             </div>
-            <small class="text-truncate"><i class="far fa-calendar-alt text-primary me-2"></i>Date Line: ${job.updatedDate}</small>
+            <small class="text-truncate"><i class="far fa-calendar-alt text-primary me-2"></i>Date Line: ${
+              job.updatedDate
+            }</small>
           </div>
         </div>
       `;
-  
-      // Set the content to the job item
-      jobItem.innerHTML = content;
-  
-      // Append the job item to the tab-1 container
-      document.getElementById("tab-1").appendChild(jobItem);
-    });
-  }
-  
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    fetchAndDisplayData();
+
+    // Set the content to the job item
+    jobItem.innerHTML = content;
+
+    // Append the job item to the tab-1 container
+    document.getElementById("tab-1").appendChild(jobItem);
   });
-  
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetchAndDisplayData();
+});
--- extension/public/ui/filter.js
+++ extension/public/ui/filter.js
@@ -1,21 +1,43 @@
 // filter.js
+// AnotherFile.js
+import { fetchAndDisplayData, updateJobTiles } from "./fetchapiresponse.js";
+
+// Now you can use fetchAndDisplayData and updateJobTiles in this file
+
 function filterJobs() {
-    var keyword = document.getElementById('keyword').value.toLowerCase();
-    var category = document.getElementById('category').value.toLowerCase();
-    var location = document.getElementById('location').value.toLowerCase();
-  
-    var jobCards = document.querySelectorAll('.job-card');
-  
-    jobCards.forEach(function (card) {
-      var title = card.querySelector('.card-title').innerText.toLowerCase();
-      var cardCategory = card.querySelector('.card-category').innerText.toLowerCase();
-      var cardLocation = card.querySelector('.card-location').innerText.toLowerCase();
-  
-      var titleMatch = title.includes(keyword);
-      var categoryMatch = category === 'all' || cardCategory.includes(category);
-      var locationMatch = location === 'all' || cardLocation.includes(location);
-  
-      card.style.display = titleMatch && categoryMatch && locationMatch ? 'block' : 'none';
-    });
-  }
-  
+  // Get values from input and select elements
+  var keyword = document.getElementById("keywordInput").value.toLowerCase();
+  var location = document.getElementById("locationSelect").value.toLowerCase();
+  console.log("Job Keyword:", keyword);
+  console.log("Job Location:", location);
+
+  // Fetch the API data (you need to implement this in fetchapiresponse.js)
+  fetch(
+    "https://localhost:7100/User?userId=76848a5d-1b02-4fbd-954a-0b505bb46f85"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Filter jobs based on keyword and location
+      var filteredJobs = data.filter((job) => {
+        // Implement your filtering logic here
+        var jobKeyword = job.title.toLowerCase();
+        var jobLocation = job.location.toLowerCase();
+
+        return (
+          jobKeyword.includes(keyword) &&
+          (location === "location" || jobLocation === location)
+        );
+      });
+
+      // Update job tiles with filtered data
+      updateJobTiles(filteredJobs);
+      // Print filtered jobs to the console
+      console.log("Filtered Jobs:", filteredJobs);
+    })
+    .catch((error) => console.error("Error fetching API:", error));
+}
+// filter.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("searchButton").addEventListener("click", filterJobs);
+});
