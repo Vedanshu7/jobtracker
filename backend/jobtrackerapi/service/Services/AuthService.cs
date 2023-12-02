de35a7909a8690805eb9aebeb466f51097849ea0
Rahul Trivedi <trivedirahul1999@gmail.com>
2023-12-02 17:39:10 -0500

    Added comments and refactoring to Services
    Added comments and refactoring to Services

--- backend/jobtrackerapi/service/Services/AuthService.cs
+++ backend/jobtrackerapi/service/Services/AuthService.cs
@@ -2,30 +2,39 @@
 using repository.Interface;
 using service.Interface;
 using service.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace service.Services
 {
+    /// <summary>
+    /// Service class responsible for authentication-related operations.
+    /// </summary>
     public class AuthService : IAuthService
     {
-        private readonly IAuthRepository authRepostiory;
+        private readonly IAuthRepository authRepository;
 
         private readonly IMapper _mapper;
 
-
+        /// <summary>
+        /// Constructor for the AuthService class.
+        /// </summary>
+        /// <param name="_repository">An instance of the IAuthRepository interface for authentication operations.</param>
+        /// <param name="mapper">An instance of the IMapper interface for object mapping.</param>
         public AuthService(IAuthRepository _repository, IMapper mapper)
         {
-            this.authRepostiory = _repository;
+            this.authRepository = _repository;
             this._mapper = mapper;
         }
 
+        /// <summary>
+        /// Authenticates a user based on Single Sign-On information.
+        /// </summary>
+        /// <param name="singleSignOn">SingleSignOn object containing authentication information.</param>
+        /// <returns>Returns a Task<User> representing the authenticated user in the service layer.</returns>
         public async Task<User> auth(SingleSignOn singleSignOn)
         {
-            return _mapper.Map<service.Models.User> (authRepostiory.auth(_mapper.Map<database.Models.SingleSignOn>(singleSignOn)));
+            // Use AutoMapper to map the SingleSignOn from the service layer to the database layer,
+            // then perform authentication and map the result back to the service layer User model.
+            return _mapper.Map<service.Models.User>(authRepository.auth(_mapper.Map<database.Models.SingleSignOn>(singleSignOn)));
         }
     }
 }
--- backend/jobtrackerapi/service/Services/UserService.cs
+++ backend/jobtrackerapi/service/Services/UserService.cs
@@ -1,45 +1,57 @@
 ﻿using AutoMapper;
 using repository.Interface;
-using repository.Repositories;
 using service.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace service.Services
 {
+    /// <summary>
+    /// Service class responsible for managing user-related operations.
+    /// </summary>
     public class UserService : IUserService
     {
-        private readonly IUserRepository userRepostiory;
-
+        private readonly IUserRepository userRepository;
         private readonly IJobRepository jobRepository;
-
         private readonly IMapper _mapper;
 
-
+        /// <summary>
+        /// Constructor for the UserService class.
+        /// </summary>
+        /// <param name="_repository">An instance of the IUserRepository interface for user-related operations.</param>
+        /// <param name="mapper">An instance of the IMapper interface for object mapping.</param>
+        /// <param name="jobRepository">An instance of the IJobRepository interface for job-related operations.</param>
         public UserService(IUserRepository _repository, IMapper mapper, IJobRepository jobRepository)
         {
-            this.userRepostiory = _repository;
+            this.userRepository = _repository;
             this._mapper = mapper;
             this.jobRepository = jobRepository;
         }
 
+        /// <summary>
+        /// Retrieves all users from the repository.
+        /// </summary>
+        /// <returns>Returns a Task<List<User>> representing a list of all users.</returns>
         public async Task<List<User>> GetAll()
         {
-            var user =  userRepostiory.GetUsers();
+            // Retrieve all users from the userRepository.
+            var users = userRepository.GetUsers();
 
-            return _mapper.Map<List<User>>(user);
+            // Map the retrieved users to a List<User> using AutoMapper.
+            return _mapper.Map<List<User>>(users);
         }
 
-
-        public async Task<List<Job>> GetUserJobs(Guid userId){
+        /// <summary>
+        /// Retrieves jobs associated with a specific user.
+        /// </summary>
+        /// <param name="userId">Unique identifier of the user.</param>
+        /// <returns>Returns a Task<List<Job>> representing a list of jobs associated with the user.</returns>
+        public async Task<List<Job>> GetUserJobs(Guid userId)
+        {
+            // Retrieve jobs associated with the user from the jobRepository.
             var jobs = jobRepository.GetUserJobs(userId);
 
+            // Map the retrieved jobs to a List<Job> using AutoMapper.
             return _mapper.Map<List<Job>>(jobs);
         }
-
     }
 }
